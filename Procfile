741b20bf61f7fa7cc2a7a8054c788dbd15dfcb17
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2016-05-21 02:00:05 +0200

    push to heroku again

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eyerisk/wsgi --log-file -
--- eyerisk/wsgi.py
+++ eyerisk/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eyerisk.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eyerisk.settings.prod")
 
 application = get_wsgi_application()
