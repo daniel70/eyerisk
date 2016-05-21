c7767fa88fcd7f734caf5aa8bb4370637e70ec8c
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2016-05-21 17:13:25 +0200

    more static settings

--- eyerisk/settings/prod.py
+++ eyerisk/settings/prod.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # test for Heroku
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # STATIC_URL = '/static/'
 #
 # # Extra places for collectstatic to find static files.
