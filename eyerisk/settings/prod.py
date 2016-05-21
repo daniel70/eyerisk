19447a62db87354a24093d9f58667d4bc9a2d5c0
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2016-05-21 17:21:26 +0200

    more static settings

--- eyerisk/settings/prod.py
+++ eyerisk/settings/prod.py
@@ -1,18 +1,8 @@
 from .base import *
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# test for Heroku
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
-#
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
 DEBUG = False
 ALLOWED_HOSTS = [".herokuapp.com", ]
+MIDDLEWARE_CLASSES.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
