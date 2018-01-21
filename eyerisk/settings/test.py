c3db4e71ed7d05c0017def01fd2dbc1552f6a043
anitaheiblom <anitaheiblom@gmail.com>
2018-01-21 16:27:09 +0100

    Testen of python 3.6.4 werkt op heroku.

--- eyerisk/settings/test.py
+++ eyerisk/settings/test.py
@@ -11,5 +11,8 @@ INSTALLED_APPS += [
 ]
 MIDDLEWARE_CLASSES.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 # end debug toolbar settings
-
+MIDDLEWARE_CLASSES.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #DATABASES = {'default': dj_database_url.parse(url='postgres://eyerisk:waterzeug@localhost/eyerisk', conn_max_age=600)}
