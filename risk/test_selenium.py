0bc4b2350d4bfc0db8052222ef6899fe0b7804f6
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2018-03-18 19:24:12 +0100

    selenium tests en fixtures toegevoegd

--- risk/test_selenium.py
+++ risk/test_selenium.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class MySeleniumTests(StaticLiveServerTestCase):
-    # fixtures = ['user-data.json']
+    fixtures = ['users.json']
 
     @classmethod
     def setUpClass(cls):
@@ -17,9 +18,27 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/account/login/'))
+        admin = User.objects.get(username='daniel')
+
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
         username_input = self.selenium.find_element_by_name("auth-username")
         username_input.send_keys('daniel')
         password_input = self.selenium.find_element_by_name("auth-password")
-        password_input.send_keys('wrongpassword')
-        self.selenium.find_element_by_xpath('//button[@value="Next"]').click()
+        password_input.send_keys('unittestpassword')
+        self.selenium.find_element_by_xpath("//button[@type='submit']").click()
+
+        # create a standard that is active
+        self.selenium.get(self.live_server_url + '/admin/risk/standard/add/')
+        name = self.selenium.find_element_by_name("name")
+        name.send_keys('Cobit 5')
+        name.submit()
+
+        # create a standard that is not active
+        self.selenium.get(self.live_server_url + '/admin/risk/standard/add/')
+        name = self.selenium.find_element_by_name("name")
+        name.send_keys('Cobit 4')
+        name.submit()
+
+        self.selenium.get(self.live_server_url + '/admin/risk/standard/')
+        self.selenium.implicitly_wait(100)
+
